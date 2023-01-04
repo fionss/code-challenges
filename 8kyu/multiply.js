88aa5784d09bab1edb6c34e28e4fc7c6bbb3bdfa
Fiona Attah <fionaattah@lads.local>
2023-01-03 21:00:21 -0500

    added new codewars challenge

---
+++ 8kyu/multiply.js
@@ -0,0 +1,10 @@
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//   a * b
+// }
+
+// My solution
+// multiply = function (a, b) {
+//   return a * b;
+// }

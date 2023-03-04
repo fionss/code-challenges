265db376a56c05ccb89390fa26cd59b290eb4650
Fiona Attah <princessfiona111@gmail.com>
2023-03-03 20:14:05 -0500

    new

--- 8kyu/hextodecimal.js
+++ 8kyu/hextodecimal.js
@@ -5,3 +5,16 @@ function hexToDec(hexString){
   //your code here
   return parseInt(hexString, 16);
 }
+
+/*
+
+let vals = [5,4,1,2,9];
+let sum = 0;
+for (let i =0; i < vals.length; i++){
+  sum += vals[i];
+}
+
+for (let val of vals){
+  sum += vals
+}
+*/

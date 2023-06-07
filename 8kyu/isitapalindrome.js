a765675b114958bf781eb1c878e668dbe79f72e3
Fiona Attah <fiona.attah@yahoo.com>
2023-06-07 12:20:45 -0400

    1 file added

---
+++ 8kyu/isitapalindrome.js
@@ -0,0 +1,7 @@
+// Write a function that checks if a given string (case insensitive) is a palindrome. A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar, the date and time 12/21/33 12:21, and the sentence: "A man, a plan, a canal – Panama".
+
+// My Solution
+function isPalindrome(x) {
+  // your code here
+  return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false;
+}

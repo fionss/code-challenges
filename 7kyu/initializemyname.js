577f6572e4f8eb13e2def0d135eb6d3340987878
Fiona Attah <fiona.attah@yahoo.com>
2023-10-02 17:52:14 -0400

    1 file added

---
+++ 7kyu/initializemyname.js
@@ -0,0 +1,19 @@
+/* Some people just have a first name; some people have first and last names and some people have first, middle and last names.
+
+You task is to initialize the middle names (if there is any).
+
+Examples
+'Jack Ryan'                   => 'Jack Ryan'
+'Lois Mary Lane'              => 'Lois M. Lane'
+'Dimitri'                     => 'Dimitri'
+'Alice Betty Catherine Davis' => 'Alice B. C. Davis' 
+
+My Solution */
+
+function initializeNames(name){
+  // Insert your brilliant code here
+  var arr = name.split(' ')
+  for (var i = 1; i < arr.length - 1; i++)
+    arr[i] = arr[i].charAt(0) + '.';
+   return arr.join(' ');
+}

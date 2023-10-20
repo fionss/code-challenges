ac66efbdc7feefeb0f02eb548a8786c6dad1cf87
Fiona Attah <fiona.attah@yahoo.com>
2023-10-20 14:31:00 -0400

    1 file added

--- 8kyu/square(n)sum.js
+++ 8kyu/square(n)sum.js
@@ -1,23 +1,23 @@
-Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
-For example, for [1, 2, 2] it should return 9 because 
-1
-2
-+
-2
-2
-+
-2
-2
-=
-9
-1 
-2
- +2 
-2
- +2 
-2
- =9.
+// For example, for [1, 2, 2] it should return 9 because 
+// 1
+// 2
+// +
+// 2
+// 2
+// +
+// 2
+// 2
+// =
+// 9
+// 1 
+// 2
+//  +2 
+// 2
+//  +2 
+// 2
+//  =9.
 
 // My SOlution
 
